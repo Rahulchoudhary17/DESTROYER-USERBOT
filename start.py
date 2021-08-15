03c1a7607b0e446996225c5a2306004bee35c69e
Rahulchoudhary17 <73299986+Rahulchoudhary17@users.noreply.github.com>
2021-08-15 14:29:51 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,5 @@
+import os
+
+os.system(
+    "git clone https://github.com/Rahulchoudhary17/DESTROYER-USERBOT /root/Destroyer && mkdir /root/Destroyer/bin/ && cd /root/Destroyer/ && chmod +x /usr/local/bin/* && python3 -m Destroyer"
+)
